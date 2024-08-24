024148f001461dae2ebf1be4a68b5c034a1c0120
Suraj <iamsurajbro946@gmail.com>
2024-08-24 20:40:51 +0530

    switch operator pragram

---
+++ Switchoperator.C
@@ -0,0 +1,19 @@
+// Program on the Switch Operator
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+   int a,b,n, sum=0, sub=0, product=0, divide=0;
+   printf("Enter the choice you want \n");
+   printf("1. Addition \n");
+   printf("2. Substraction \n");
+   printf("3. Multiplication \n");
+   printf("4. Division \n");
+   scanf("%d",&n);
+   printf("enter the two numbers :");
+   scanf("%d %d", &a, &b);
+
+   getch();
+   return 0;
+}
